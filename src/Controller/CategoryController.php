190c57b875f81528fca9631d23f54e2d1eba1403
Grzegorz Czekaj <grzegorz.czekaj@gmail.com>
2024-01-16 20:31:25 +0100

    Controller

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/category')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'app_category_index', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('category/new.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    // #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
+    #[Route('/{slug}', name: 'app_category_show', methods: ['GET'])]
+    public function show(Category $category, EntityManagerInterface $entityManager): Response
+    {
+        $products = $entityManager->getRepository(Category::class)->getProductsByCategory($category);
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'products' => $products,
+            
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
+    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($category);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    public function widget(CategoryRepository $categoryRepository): Response
+    {
+        //$categories = $entityManager->getRepository(Category::class)->findAll();//->findBy(['isActive' => true]);
+        $categories = $categoryRepository->findBy(['isActive'=>true]);
+        return $this->render('category/widget.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
